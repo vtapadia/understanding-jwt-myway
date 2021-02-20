035850c76789cd531da3ae218e49452617319bf5
Varesh Tapadia <varesh.tapadia@rabobank.nl>
2021-02-20 01:24:34 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+The project is a sample implementation of my understanding of JWT tokens and its implementation 
+
---
+++ src/main/java/com/github/vtapadia/example/jwt/UnderstandingJwtMywayApplication.java
@@ -0,0 +1,13 @@
+package com.github.vtapadia.example.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingJwtMywayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderstandingJwtMywayApplication.class, args);
+    }
+
+}
